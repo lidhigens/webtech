6b5da56693f7da8b4e050bd5491cbdde1121447b
lidhigens1 <dragonlordofthesky@yahoo.com>
2015-09-15 19:29:20 +0000

    updated version

--- introduction.js
+++ introduction.js
@@ -8,5 +8,5 @@ var classYears = [
     "Senior", 
     "Alumni"];
 var message = 
-    "Hello! My name is "+name+". I'm "+age+" years old, and I am a "+classYears[2]+' '+major+" at Union College.";
+    "Hello! My name is "+name+". I'm "+age+" years old, and I am a "+classYears[2]+' '+major+" major at Union College.";
 console.log(message);
